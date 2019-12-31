a454391a9bcce6acbbe0e770e6258bda7ede0564
Addison Wen <addison@Addisons-MacBook-Pro.local>
2019-12-31 12:10:57 +1100

    Added resetpassword, bootcamp & course ownership check

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' })
 
 const Bootcamp = require('./models/Bootcamp')
 const Course = require('./models/Course')
+const User = require('./models/User')
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -26,6 +27,10 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 )
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+)
+
 // Import into DB
 const importData = async () => {
   try {
@@ -33,6 +38,8 @@ const importData = async () => {
     console.log('Bootcamps Imported...'.green.inverse)
     await Course.create(courses)
     console.log('Courses Imported...'.green.inverse)
+    await User.create(users)
+    console.log('Users Imported...'.green.inverse)
   } catch (err) {
     console.error(err)
   } finally {
@@ -47,6 +54,8 @@ const deleteData = async () => {
     console.log('Bootcamps Destroyed...'.red.inverse)
     await Course.deleteMany()
     console.log('Courses Destroyed...'.red.inverse)
+    await User.deleteMany()
+    console.log('Users Destroyed...'.red.inverse)
   } catch (err) {
     console.error(err)
   } finally {
